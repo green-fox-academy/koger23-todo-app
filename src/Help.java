6995d66f806db61e32914249efc0d517daaa194a
Gergely Kocsis <koger23@gmail.com>
2019-04-18 23:26:30 +0200

    feat(Help): argument list extended with longhand commands and new commands

--- src/Help.java
+++ src/Help.java
@@ -1,9 +1,11 @@
 public class Help {
   public static void printHelp() {
     System.out.println("Command line arguments:");
-    System.out.println("-l   Lists all the tasks");
-    System.out.println("-a   Adds a new task");
-    System.out.println("-r   Removes an task");
-    System.out.println("-c   Completes an task");
+    System.out.println("-l,  --list        Lists all unchecked tasks");
+    System.out.println("-la, --listall     Lists all the tasks");
+    System.out.println("-a,  --add         Adds a new task");
+    System.out.println("-r,  --remove      Removes a task by index");
+    System.out.println("-rd, --removedone  Remove all done task");
+    System.out.println("-c,  --check       Completes an task");
   }
 }
