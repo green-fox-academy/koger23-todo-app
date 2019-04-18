c76150a9fd9961d713b2bb299914f35383dfb173
Gergely Kocsis <koger23@gmail.com>
2019-04-18 12:11:09 +0200

    feat(TaskList): init

---
+++ src/TaskList.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+
+  private List<Task> taskList = new ArrayList<>();
+
+  public TaskList() {
+  }
+
+  public List<Task> getTaskList() {
+    return taskList;
+  }
+
+  public void setTaskList(List<Task> taskList) {
+    this.taskList = taskList;
+  }
+
+  public void addTask(Task task){
+
+  }
+
+  public void removeTask(Task task){
+
+  }
+
+  public void removeTask(int index){
+
+  }
+
+  public void checkTask(int index){
+    
+  }
+
+}
