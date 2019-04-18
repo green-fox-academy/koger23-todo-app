86164a75c01bf3932eb094775bb9b7eb888c4cce
Gergely Kocsis <koger23@gmail.com>
2019-04-18 11:55:03 +0200

    feat(Task): variables, constructor, getters-setters added.

--- src/Task.java
+++ src/Task.java
@@ -1,2 +1,26 @@
 public class Task {
+
+  boolean isFinished;
+  String taskDescription;
+
+  public Task(boolean isFinished, String taskDescription) {
+    this.isFinished = isFinished;
+    this.taskDescription = taskDescription;
+  }
+
+  public boolean isFinished() {
+    return isFinished;
+  }
+
+  public void setFinished(boolean finished) {
+    isFinished = finished;
+  }
+
+  public String getTaskDescription() {
+    return taskDescription;
+  }
+
+  public void setTaskDescription(String taskDescription) {
+    this.taskDescription = taskDescription;
+  }
 }
