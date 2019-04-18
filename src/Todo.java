252ecb43a99bedf5dd7ba38102c2f5f7a1e89e13
Gergely Kocsis <koger23@gmail.com>
2019-04-18 14:01:16 +0200

    feat(Todo): listing tasks works

--- src/Todo.java
+++ src/Todo.java
@@ -1,13 +1,26 @@
 public class Todo {
 
+
+  private static TaskList taskListObj = new TaskList();
+
+  public Todo() {
+
+  }
+
   public static void main(String[] args) {
 
     if (args.length == 0) {
-      System.out.println("No args");
-    } else {
-      for (int i = 0; i < args.length; i++) {
-        System.out.println(args[i]);
-      }
+      System.out.println("Command Line Todo application");
+      System.out.println("=============================\n");
+      System.out.println("Command line arguments:");
+      System.out.println("-l   Lists all the tasks");
+      System.out.println("-a   Adds a new task");
+      System.out.println("-r   Removes an task");
+      System.out.println("-c   Completes an task");
+
+    } else if (args[0].equals("-l")) {
+      taskListObj.printList();
     }
   }
 }
+
