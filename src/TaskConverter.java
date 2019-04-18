cdabbb15e2d6143db1590f49fbfa0cdd91f6d806
Gergely Kocsis <koger23@gmail.com>
2019-04-18 13:24:16 +0200

    feat(TaskConverter): modified convertDataToTask() param type.

--- src/TaskConverter.java
+++ src/TaskConverter.java
@@ -1,2 +1,20 @@
+import java.util.List;
+
 public class TaskConverter {
+
+
+  public static Task convertDataToTask(String text, String separator){
+
+    String[] arr = text.split(separator);
+
+    Task task  = new Task(Boolean.parseBoolean(arr[0]), arr[1]);
+
+    return task;
+  }
+
+  public static List<String> convertDataToString(Task task){
+
+    return null;
+  }
+
 }
