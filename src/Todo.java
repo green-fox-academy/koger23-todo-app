3d31352ea1784250de2d891f834c8321c697cdc0
Gergely Kocsis <koger23@gmail.com>
2019-04-18 17:35:34 +0200

    refactor(Todo): new class for operating with args: ArgHandler

--- src/Todo.java
+++ src/Todo.java
@@ -1,54 +1,9 @@
 public class Todo {
-  private static TaskList taskListObj = new TaskList();
-
   public Todo() {
   }
 
   public static void main(String[] args) {
-    if (args.length == 0) {
-      System.out.println("Command Line Todo application");
-      System.out.println("=============================\n");
-
-      Help.printHelp();
-    } else if (args[0].equals("-la")) {
-      taskListObj.printList();
-    } else if (args[0].equals("-l")) {
-      taskListObj.printListUndoneOnly();
-    } else if (args[0].equals("-a")) {
-      try {
-        taskListObj.addTask(args[1]);
-      } catch (ArrayIndexOutOfBoundsException e) {
-        System.out.println("Unable to add: no task provided.\n");
-        System.out.println("Example: Todo -a 'new task'\n");
-        Help.printHelp();
-      }
-    } else if (args[0].equals("-r")) {
-      try {
-        taskListObj.removeTask(Integer.valueOf(args[1]) - 1);
-      } catch (ArrayIndexOutOfBoundsException e) {
-        System.out.println("Unable to remove: no index provided.\n");
-      } catch (NumberFormatException e) {
-        System.out.println("Unable to remove: index is not a number");
-      }
-    } else if (args[0].equals("-ru")) {
-      try {
-        taskListObj.removeDoneTasks();
-      } catch (ArrayIndexOutOfBoundsException e) {
-        System.out.println("Unable to remove: no index provided.\n");
-      } catch (NumberFormatException e) {
-        System.out.println("Unable to remove: index is not a number");
-      }
-    } else if (args[0].equals("-c")) {
-      try {
-        taskListObj.checkTask(Integer.valueOf(args[1]) - 1);
-      } catch (ArrayIndexOutOfBoundsException e) {
-        System.out.println("Unable to remove: no index provided.\n");
-      } catch (NumberFormatException e) {
-        System.out.println("Unable to remove: index is not a number");
-      }
-    } else {
-      System.out.println("Unsupported argument");
-    }
+    ArgHandler.checkArgs(args);
   }
 }
 
