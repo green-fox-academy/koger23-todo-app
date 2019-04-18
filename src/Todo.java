d63c2627a1e706cca23447a91165d9d6e1ebfafa
Gergely Kocsis <koger23@gmail.com>
2019-04-18 14:25:39 +0200

    feat(Todo): -a argument for add task works

--- src/Todo.java
+++ src/Todo.java
@@ -12,15 +12,31 @@ public class Todo {
     if (args.length == 0) {
       System.out.println("Command Line Todo application");
       System.out.println("=============================\n");
-      System.out.println("Command line arguments:");
-      System.out.println("-l   Lists all the tasks");
-      System.out.println("-a   Adds a new task");
-      System.out.println("-r   Removes an task");
-      System.out.println("-c   Completes an task");
+      printHelp();
 
     } else if (args[0].equals("-l")) {
       taskListObj.printList();
+
+    } else if (args[0].equals("-a")) {
+
+      try{
+        taskListObj.addTask(args[1]);
+
+      } catch (ArrayIndexOutOfBoundsException e){
+        System.out.println("You have not defined the task description.\n");
+        System.out.println("Example: Todo -a 'new task'\n");
+        printHelp();
+      }
+
     }
+  }
+
+  public static void printHelp(){
+    System.out.println("Command line arguments:");
+    System.out.println("-l   Lists all the tasks");
+    System.out.println("-a   Adds a new task");
+    System.out.println("-r   Removes an task");
+    System.out.println("-c   Completes an task");
   }
 }
 
