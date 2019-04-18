92bc2b12455f8f5919330f41f766b63ba450006a
Gergely Kocsis <koger23@gmail.com>
2019-04-18 16:47:48 +0200

    style(TaskConverter): reformat code according to the style guide

--- src/TaskConverter.java
+++ src/TaskConverter.java
@@ -2,30 +2,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TaskConverter {
-
-
   public static Task convertDataToTask(String text, String separator){
-
     String[] arr = text.split(separator);
-
     Task task  = new Task(Boolean.parseBoolean(arr[1]), arr[0]);
-
     return task;
   }
 
   public static List<String> convertDataToString(List<Task> taskList, String separator){
-
     List<String> taskStrings = new ArrayList<>();
-
     for (Task task : taskList){
-
       String taskString = task.getTaskDescription() + separator + task.isFinished();
-
       taskStrings.add(taskString);
-
     }
-
     return taskStrings;
   }
-
 }
