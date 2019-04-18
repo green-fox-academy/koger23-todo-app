5a6fd33ddc8de146eff1fc515859294d9c26430e
Gergely Kocsis <koger23@gmail.com>
2019-04-18 16:45:24 +0200

    style(DataHandler): reformat code according to the style guide

--- src/DataHandler.java
+++ src/DataHandler.java
@@ -6,33 +6,24 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class DataHandler {
-
   private List<String> data;
   private Path path;
   private String FILENAME = "todo.csv";
   private final String sep = ";";
 
   public DataHandler() {
-
     path = Paths.get(FILENAME);
     File dataFile = new File(FILENAME);
-
     if (dataFile.isFile()){
-
       try {
         data = Files.readAllLines(path);
-//        System.out.println("Datafile exists, read in.");
-
       } catch (IOException e) {
         System.out.println("Error while reading data file.");
         e.printStackTrace();
       }
-
     } else {
       try {
         dataFile.createNewFile();
-//        System.out.println("Datafile did not exist, so it has been created.");
-
       } catch (IOException e) {
         System.out.println("I/O Error while creating data file.");
         e.printStackTrace();
@@ -41,18 +32,15 @@ public class DataHandler {
   }
 
   public void saveDatas(List<String> taskList){
-
     try {
       Files.write(path, taskList);
     } catch (IOException e) {
       System.out.println("Error during saving tasks.");
       e.printStackTrace();
     }
-
   }
 
   public void loadDatas(){
-
   }
 
   public List<String> getData(){
