a7dd78fb7a5435f6a04b75b1aac13fed7c375a9b
Gergely Kocsis <koger23@gmail.com>
2019-04-19 15:44:24 +0200

    feat(User): isActive property and getters-setters added.

--- src/User.java
+++ src/User.java
@@ -1,2 +1,25 @@
 public class User {
+  private String userName;
+  private boolean isActive;
+
+  public User(boolean isActive, String userName) {
+    this.userName = userName;
+    this.isActive = isActive;
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public boolean isActive() {
+    return isActive;
+  }
+
+  public void setActive(boolean active) {
+    isActive = active;
+  }
 }
