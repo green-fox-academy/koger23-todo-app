b24f9e8561005085b95fded0e103aa2ca6bc41f4
Gergely Kocsis <koger23@gmail.com>
2019-04-19 18:55:09 +0200

    feat(UserFileHandler): constructor and saveDatas() added.

--- src/UserFileHandler.java
+++ src/UserFileHandler.java
@@ -1,11 +1,35 @@
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 
 public class UserFileHandler {
+  private List<String> data;
   private static Path path;
+  private String FILENAME = "users.csv";
+
+  public UserFileHandler() {
+    path = Paths.get(FILENAME);
+    File dataFile = new File(FILENAME);
+    if (dataFile.isFile()){
+      try {
+        data = Files.readAllLines(path);
+      } catch (IOException e) {
+        System.out.println("Error while reading userdata file.");
+        e.printStackTrace();
+      }
+    } else {
+      try {
+        dataFile.createNewFile();
+      } catch (IOException e) {
+        System.out.println("I/O Error while creating userdata file.");
+        e.printStackTrace();
+      }
+    }
+  }
 
   public static void saveDatas(List<String> taskList){
     try {
@@ -22,5 +46,9 @@ public class UserFileHandler {
 
   public static void setPath(Path path) {
     UserFileHandler.path = path;
+  }
+
+  public List<String> getData(){
+    return data;
   }
 }
